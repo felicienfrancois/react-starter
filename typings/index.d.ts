2e87b8d592041fbe8906b5e2a12f084dbe9bff90
Félicien François <felicien@tweakstyle.com>
2016-05-25 19:18:35 +0200

    remove typings from versioning

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/react-addons-test-utils/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
-/// <reference path="globals/webpack-env/index.d.ts" />
